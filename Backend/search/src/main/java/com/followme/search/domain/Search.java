e58169954e719e06dbc6b7ab3d9fc395f055cb59
andoneKwon <wlsdn110@gmail.com>
2020-09-05 03:32:39 +0900

    Elasticsearch config 생성자주입으로 변경.

--- Backend/search/src/main/java/com/followme/search/domain/Search.java
+++ Backend/search/src/main/java/com/followme/search/domain/Search.java
@@ -3,20 +3,23 @@ package com.followme.search.domain;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
 public class Search {
-    private String category;
+    private int category;
     private float grade_avg;
-    private String menu;
     private String shopname;
     private int likenum;
     private String address;
-    private int reviewnum;
-    private String introduce;
 
-    public Search(String category,float grade_avg,String menu,String shopname,int likenum,String address,int reviewnum,String introduce){
-
+    public Search(int category,float grade_avg,String shopname,int likenum,String address){
+        this.shopname = shopname;
+        this.address=address;
+        this.category=category;
+        this.grade_avg=grade_avg;
+        this.likenum=likenum;
     }
 }
--- Backend/search/src/main/java/com/followme/search/service/SearchService.java
+++ Backend/search/src/main/java/com/followme/search/service/SearchService.java
@@ -2,31 +2,34 @@ package com.followme.search.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.followme.search.config.ElasticsearchConf;
 import com.followme.search.domain.FullQuery;
 import com.followme.search.domain.Query;
 import com.followme.search.domain.QueryString;
+import com.followme.search.domain.Search;
 import com.followme.search.web.dto.SearchResponseDto;
 import com.google.gson.Gson;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.nio.entity.NStringEntity;
 import org.apache.http.util.EntityUtils;
 import org.elasticsearch.client.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@NoArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class SearchService {
+    final private ElasticsearchConf elasticsearchConf;
     public JsonNode SearchDoc(int from,String target) throws IOException {
-        final SearchResponseDto searchResponseDto;
-        RestClient restClient = RestClient.builder(new HttpHost("3.15.22.4",9200,"http")).build();
-
         Map<String,Object> result = new HashMap<>();
 
         QueryString queryString = new QueryString(target);
@@ -43,7 +46,7 @@ public class SearchService {
         Request request = new Request("POST","/infos_table/_search");
         request.addParameter("pretty","true");
         request.setEntity(httpEntity);
-        Response response = restClient.performRequest(request);
+        Response response = elasticsearchConf.getRestClient().performRequest(request);
         String responseBody = EntityUtils.toString(response.getEntity());
 
         ObjectMapper mapper = new ObjectMapper();
@@ -52,7 +55,14 @@ public class SearchService {
         JsonNode document2;
         document2=document.get("hits").get("hits");
 
+        elasticsearchConf.getRestClient().close();
+/*
+        search.setAddress(document2.get("_source").get("address").toString());
+        search.setCategory(document2.get("_source").get("catagory").toString());
+*/
         return document2;
     }
+
+
 
 }
