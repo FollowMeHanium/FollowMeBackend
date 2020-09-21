a5940c60ee5c32e7cce47f2240f2daef80ef4784
Andone <wlsdn110@gmail.com>
2020-09-21 15:57:00 +0900

    Api gateway

---
+++ Backend/Apigateway/src/main/java/com/followme/apigateway/Controller/ApiContorller.java
@@ -0,0 +1,4 @@
+package com.followme.apigateway.Controller;
+
+public class ApiContorller {
+}
---
+++ Backend/Apigateway/src/main/java/com/followme/apigateway/PreFilter.java
@@ -0,0 +1,47 @@
+package com.followme.apigateway;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class PreFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return false;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+
+        String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (!validateToken(authorizationHeader)) {
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseBody("API key not authorized");
+            ctx.getResponse().setHeader("Content-Type", "text/plain;charset=UTF-8");
+            ctx.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+        }
+        return null;
+    }
+
+    private boolean validateToken(String tokenHeader) {
+        System.out.println("access");
+        return true;
+    }
+}
