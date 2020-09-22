457a10716e09bc565d419e888332a8edcd96c406
Andone <wlsdn110@gmail.com>
2020-09-22 20:48:05 +0900

    check commit

--- Backend/authApi/routes/auth.js
+++ Backend/authApi/routes/auth.js
@@ -164,8 +164,8 @@ router.post('/checkEmail',function(req,res,next){
 router.post('/getNewToken',function(req,res,next){
   var {authorization,refreshToken}=req.header;
   var tokenValue=jwt.decode(authorization);
-  var userId = tokenValue.body.user_id;
   console.log(tokenValue.body.gender);
+  var userId = tokenValue.body.user_id;
   client.get(userId,function(err,result){
     if(isEmpty(result)){
       res.json({
